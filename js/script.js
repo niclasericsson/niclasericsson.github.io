023dac7c98bc810760c4afdd0250121c6bdd8202
Niclas Ericsson <niclas.ericsson@live.se>
2017-11-19 18:52:51 +0100

    Fixes again

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
 		console.log($(this).val());
 		//$(this).find(".projectInfo").fadeIn();
 		if($(this).attr("data-status") == "closed"){
+			$(this).children(".labels").show();
 			$(this).children(".projectInfo").show();
 			$(this).attr("data-status", "open");
 		}
 		else{
+			$(this).children(".labels").hide();
 			$(this).children(".projectInfo").hide();
 			$(this).attr("data-status", "closed");
 		}
