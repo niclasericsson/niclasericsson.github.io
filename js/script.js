b051a7a689b3a0fa6bd153fb92cf21a99218885c
Niclas Ericsson <niclas.ericsson@live.se>
2017-11-19 16:39:54 +0100

    More stuff

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-/*$(document).ready(function() {
+$(document).ready(function() {
 
 	$(".infoBox").on("click", function(){
-		if($(this).height() < 450){
+		/*if($(this).height() < 450){
 			$(this).height("450px");
 			$(this).children(".projectInfo").css({"visibility": "visible"});
 			$(this).children(".projectInfo").css({"opacity": 100});
@@ -10,31 +10,7 @@
 			$(this).height("110px");
 			$(this).children(".projectInfo").css({"opacity": 0});
 			$(this).children(".projectInfo").css({"visibility": "hidden"});
-		}
+		}*/
 	});
 	
-	$("#scrollDownImage").click(function() {
-		$('html, body').animate({
-			scrollTop: $(".projectTitle").offset().top
-		}, 1000);
-	});
-	
-	$(window).scroll(function(){
-
-		if ($(this).scrollTop() > 1) {
-			$('.scrollDown').fadeOut();
-		}
-		else {
-		   if ($(this).scrollTop() < 1) {
-			   $('.scrollDown').fadeIn();
-		   } else {
-			   $('.scrollDown').fadeOut();
-		   }
-		}
-
-	});
-	
-	if($(window).width() > 1366)
-		$(".imageBox").css({"margin-top" : "200px"})
-	
-});*/
+});
