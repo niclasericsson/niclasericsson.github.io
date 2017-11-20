dabcea9b7d3a72331d1b3f6c4387dddd60b936f8
Niclas Ericsson <niclas.ericsson@live.se>
2017-11-20 18:14:47 +0100

    More stuff

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
 
 	$(".infoBox").on("click", function(){
-		console.log($(this).val());
-		//$(this).find(".projectInfo").fadeIn();
+
 		if($(this).attr("data-status") == "closed"){
-			$(this).children(".labels").show();
-			$(this).children(".projectInfo").show();
+
+			// Close open boxes
+			$(this).siblings(".infoBox").attr("data-status", "closed");
+			$(this).siblings(".infoBox").children(".labels").hide();
+			$(this).siblings(".infoBox").children(".projectInfo").hide();
+
+			// Open box
+			$(this).children(".labels").slideToggle('fast');
+			$(this).children(".projectInfo").slideToggle('fast');
 			$(this).attr("data-status", "open");
 		}
 		else{
-			$(this).children(".labels").hide();
-			$(this).children(".projectInfo").hide();
+			$(this).children(".labels").slideToggle('fast');
+			$(this).children(".projectInfo").slideToggle('fast');
 			$(this).attr("data-status", "closed");
 		}
+		
 	});
 	
 });
