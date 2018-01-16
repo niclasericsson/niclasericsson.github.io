24949df22e00b9a79d0578678f196965e06ff727
Niclas Ericsson <niclas.ericsson@live.se>
2018-01-16 12:05:45 +0100

    Added StudyAlong presentation site

---
+++ studyalong/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $('.mainImage').animate({'opacity':'1'}, 1000);
+
+	$(window).scroll( function(){
+
+        $('.image').each( function(i){
+        	
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500);
+            }
+
+        }); 
+
+    });
+
+});
