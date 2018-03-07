a2a7dc29bddafe7554473b34b41dc4462f2b36c3
Niclas Ericsson <niclas.ericsson@live.se>
2018-03-07 11:21:31 +0100

    Updated StudyAlong, removed firebase test

--- studyalong/js/main.js
+++ studyalong/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     $('.mainImage').animate({'opacity':'1'}, 1000);
+    $('.screenShotImage').animate({'opacity':'1'}, 1000);
 
 	$(window).scroll( function(){
 
@@ -13,7 +14,24 @@ $(document).ready(function () {
                 $(this).parent().parent().animate({'opacity':'1'},500);
             }
 
-        }); 
+        });
+
+
+        if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            var bottom_of_object = $('#logoContainer').offset().top + $('#logoContainer').outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                var delay = 0;
+                $('#logoContainer').children().each( function(i){
+                    delay = delay + 50;
+                    $(this).delay(delay).animate({'opacity':'1'},500);
+                });
+            }
+        } else {
+            $('#logoContainer').children().each( function(i){
+                $(this).animate({'opacity':'1'},500);
+            });
+        }
 
     });
 
